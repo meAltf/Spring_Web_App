98d3a4c943fdff8e50e924d36fab775bc0b5b4af
meAltf <alataf.dce.cs1@gmail.com>
2025-08-06 00:33:32 +0530

    refactored

--- src/main/java/com/robert/simpleWebApp/Controller/HomeController.java
+++ src/main/java/com/robert/simpleWebApp/Controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.robert.simpleWebApp;
+package com.robert.simpleWebApp.Controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
