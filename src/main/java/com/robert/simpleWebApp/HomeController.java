4033b4e8fd728c1404bab9e2919049dcb9bc2adb
meAltf <alataf.dce.cs1@gmail.com>
2025-08-05 22:38:39 +0530

    Basic understnading of spring web

---
+++ src/main/java/com/robert/simpleWebApp/HomeController.java
@@ -0,0 +1,18 @@
+package com.robert.simpleWebApp;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String greet() {
+        return "welcome to robert world!";
+    }
+
+    @RequestMapping("/about")
+    public String about() {
+        return "all related to about!";
+    }
+}
